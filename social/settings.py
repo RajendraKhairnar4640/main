945e61100649e55e34d0fa2dc0329cf3b2a351a2
dell <rajendrakhairnar49@gmail.com>
2022-09-29 13:27:36 +0530

    ten commit

--- social/settings.py
+++ social/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%gf&fl($r+1arqg89)+-=#6r%dxd51up0p1%(_xl9hm!7s94%q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['twgram.herokuapp.com','localhost']
 
